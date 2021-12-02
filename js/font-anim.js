3653e8dadf888d4e974aa22e7f2537f9ea262cd3
Will Peix <hector96fi@yahoo.es>
2021-12-02 17:42:17 +0800

    0.122 Footer in Steroids, prettier Header
    1. Footer media query
    2. rounded improved footer
    3. prettier Header

--- js/font-anim.js
+++ js/font-anim.js
@@ -1,12 +1,12 @@
-myText.style.fontVariationSettings = "\"wght\" 550;
-myText.style.fontWeight = 550;
-// myText.style.fontStretch = "150%"
+// myText.style.fontVariationSettings = "\"wght\" 550;
+// myText.style.fontWeight = 550;
+// // myText.style.fontStretch = "150%"
 
-function randomizeText() {
-  randomWeight = Math.random() * (200 - 35) + 35;
-  randomWidth = Math.random() * (200 - 100) + 100;
-  myText.style.fontVariationSettings =
-    '"wght" ' + randomWeight + ', "wdth" ' + randomWidth;
-}
+// function randomizeText() {
+//   randomWeight = Math.random() * (200 - 35) + 35;
+//   randomWidth = Math.random() * (200 - 100) + 100;
+//   myText.style.fontVariationSettings =
+//     '"wght" ' + randomWeight + ', "wdth" ' + randomWidth;
+// }
 
-setInterval(randomizeText, 1000);
+// setInterval(randomizeText, 1000);
