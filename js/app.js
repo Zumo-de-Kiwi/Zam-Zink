f045a355420dc655e2a7d2c8b0fff0896f5f2cc5
Will Peix <hector96fi@yahoo.es>
2021-12-13 15:15:30 +0800

    ver. 0.230 Smooth toTop scroll
    1. smoth scrolling totop
    2. left-arrow rewrite global
    3. external link with blank page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-//Function to get rid the preloader
-window.addEventListener("load", () => {
-  const preloader = document.querySelector(".preloader");
-  preloader.classList.add("preload-finish");
+//Function to get rid the preloader when the pages loads completely
+window.addEventListener('load', () => {
+  const preloader = document.querySelector('.preloader');
+  preloader.classList.add('preload-finish');
+});
+
+// Smoth scrolling to a section
+const btnScrollTo = document.querySelector('.toTopBtn');
+const leftArrow = document.querySelector('.left-arrow');
+
+btnScrollTo.addEventListener('click', function (e) {
+  leftArrow.scrollIntoView({ behavior: 'smooth' });
 });
