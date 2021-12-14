fc171e471a2edc452eae88769911889ae9a45edc
Will Peix <hector96fi@yahoo.es>
2021-12-14 15:27:16 +0800

    ve. 0.340 Finish Lost Objects
    1. place all the pics and texts
    2.placing the placa

--- pages/works/lost-objects/lost-objects.js
+++
@@ -1,7 +0,0 @@
-///// Smoth scrolling, click a btn and then will got to a section of the page
-// const btnScrollTo = document.querySelector('.toTopBtn');
-// const leftArrow = document.querySelector('.left-arrow');
-
-// btnScrollTo.addEventListener('click', function (e) {
-//   leftArrow.scrollIntoView({ behavior: 'smooth' });
-// });
