6bcb643e19558b50ce2e20b5f37e87e40c604b04
Stonkol Peix <hector96fi@yahoo.es>
2021-12-20 21:54:02 +0800

    ver. 0.302 ++.gitattributes, smooth global,
    1. Add .gitattributes
    2. link next prj to analog photo
    3. set smooth scrolling to the global scss html{}

--- themes/default.js
+++ themes/default.js
@@ -1,17 +1,17 @@
 export default {
   fonts: {
-    title: "sans-serif",
-    main: "sans-serif",
+    title: 'sans-serif',
+    main: 'sans-serif',
   },
 
   colors: {
-    primary1: "#333",
+    primary1: '#333',
   },
   //for responsive design
   breakpoints: {
-    sm: "screen and (max-width: 640px)",
-    md: "screen and (max-width: 768px)",
-    lg: "screen and (max-width: 1024px)",
-    xl: "screen and (max-width: 1280px)",
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)',
   },
 };
