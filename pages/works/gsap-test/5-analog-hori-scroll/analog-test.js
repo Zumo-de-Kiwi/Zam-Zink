9690d0ca5f2faaee2ba1486deb4487772c9a746c
Stonkol Peix <hector96fi@yahoo.es>
2021-12-26 23:14:15 +0800

    ver. 0.362 trying global color changes on scroll
    1. change --line-color to text-color
    2. Added my custom header to analog-photo
    3. Trying changing the bg on horizontal scroll

--- pages/works/gsap-test/5-analog-hori-scroll/analog-test.js
+++ pages/works/gsap-test/5-analog-hori-scroll/analog-test.js
@@ -1,3 +1,80 @@
+//change colors
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// OPTION A
+// https://codepen.io/snorkltv/pen/KKvQOKY
+// const sectionColors = ['black', 'pink', 'friends-green', 'pale-blue', 'yellow'];
+// const bgColors = ['#000', '#F9D1CF', '#33D17C', '#3E44C8', '#FFF23F'];
+// const lineColors = ['#FFFF00', '#000', '#fff', '#000', '#000'];
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// OPTION B
+// from: https://codepen.io/stonkol/pen/MWEOzom?editors=1010
+// gsap.registerPlugin(ScrollTrigger);
+
+// /* SMOOTH SCROLL */
+// const scroller = new LocomotiveScroll({
+//   el: document.querySelector('.container'),
+//   smooth: true,
+// });
+
+// scroller.on('scroll', ScrollTrigger.update);
+
+// ScrollTrigger.scrollerProxy('.container', {
+//   scrollTop(value) {
+//     return arguments.length
+//       ? scroller.scrollTo(value, 0, 0)
+//       : scroller.scroll.instance.scroll.y;
+//   },
+//   getBoundingClientRect() {
+//     return {
+//       left: 0,
+//       top: 0,
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//     };
+//   },
+// });
+
+// ScrollTrigger.addEventListener('refresh', () => scroller.update());
+
+// ScrollTrigger.refresh();
+
+// /* COLOR CHANGER */
+// window.addEventListener('load', function () {
+//   const scrollColorElems = document.querySelectorAll('[data-bgcolor]');
+//   scrollColorElems.forEach((colorSection, i) => {
+//     const prevBg = i === 0 ? '' : scrollColorElems[i - 1].dataset.bgcolor;
+//     const prevText = i === 0 ? '' : scrollColorElems[i - 1].dataset.textcolor;
+
+//     ScrollTrigger.create({
+//       trigger: colorSection,
+//       scroller: '.panel', //was container in the original file
+//       start: 'top 50%',
+//       onEnter: () =>
+//         gsap.to('body', {
+//           backgroundColor: colorSection.dataset.bgcolor,
+//           color: colorSection.dataset.textcolor,
+//           overwrite: 'auto',
+//         }),
+//       onLeaveBack: () =>
+//         gsap.to('body', {
+//           backgroundColor: prevBg,
+//           color: prevText,
+//           overwrite: 'auto',
+//         }),
+//     });
+//   });
+// });
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+///// OPtion C
+//https://codepen.io/stonkol/pen/YzrEBja?editors=0110
+/////// for css
+// section.active {
+//   background-color: orange;
+// }
+
 //HORIZONTAL SCROLL
 gsap.registerPlugin(ScrollTrigger);
 const sections = document.querySelectorAll('.block');
