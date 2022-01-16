aaac833acc9c60dfc5a9647161c4537486942e19
Will Peix <hector96fi@yahoo.es>
2022-01-16 12:56:21 +0800

    ver. 0.591 x4 Modal mainDESIGN
    1. now Modal Readme, Eatme, OutofStock, pawn can be open and closed

--- pages/works/design/design.js
+++ pages/works/design/design.js
@@ -7,36 +7,76 @@ btnScroll.addEventListener('click', function (e) {
   // console.log(btnScroll, header);
 });
 
-//
-//
+/////////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
 //// Elements (listeners)////
 const pawn = document.querySelector('#pawn-open');
 const readme = document.querySelector('#read-me-open');
 const out = document.querySelector('#out-open');
 const eatme = document.querySelector('#eatme-open');
-console.log(pawn, readme, out, eatme, bg);
-/////////// MODALS ////////////
+// console.log('Listeners: ' + pawn, readme, out, eatme);
+/////////// MODALS ///// ///////
 const pawnModal = document.querySelector('#pawn-modal');
-const readmeModal = document.querySelector('#read-me-modal');
+const readmeModal = document.querySelector('#read-modal');
 const outModal = document.querySelector('#out-modal');
-const eatmeModal = document.querySelector('#eatme-modal');
-console.log(pawnModal, readmeModal, outModal, eatmeModal, bgModal);
+const eatmeModal = document.querySelector('#eat-modal');
+// const modal = document.querySelectorAll('.modal');
+console.log('Modals: ' + pawnModal, readmeModal);
+console.log('Modals: ' + outModal, eatmeModal);
 ////////// CLOSE TRIGGERS //////////
+// const xBtn = document.querySelectorAll('.x-btn');
 const bg = document.querySelector('.bg-modal');
-const xBtn = document.querySelector('.x-btn');
+const shadow = document.querySelector('.shadow-layer');
+console.log('Close triggers: ' + bg, shadow);
 
+/////////// FUNCS ///////////////
 // PAWN
 const openPawn = function () {
   pawnModal.classList.remove('hidden');
   bg.classList.remove('hidden');
-  xBtn.classList.remove('hidden');
+  shadow.classList.remove('hidden');
+  console.log('Open Pawn');
 };
-const closePawn = function () {
-  pawnModal.classList.add('hidden');
-  bg.classList.add('hidden');
-  xBtn.classList.add('hidden');
-};
-
 // READ ME
-
+const openRead = function () {
+  readmeModal.classList.remove('hidden');
+  bg.classList.remove('hidden');
+  shadow.classList.remove('hidden');
+  console.log('Open Read');
+};
+// EAT ME
+const openEat = function () {
+  eatmeModal.classList.remove('hidden');
+  bg.classList.remove('hidden');
+  shadow.classList.remove('hidden');
+  console.log('Open Eat');
+};
+// CLOSE
+// if (!modal.classList.contains('hidden'))
+const closeModal = function () {
+  if (!pawnModal.classList.contains('hidden')) {
+    pawnModal.classList.add('hidden');
+  } else if (!readmeModal.classList.contains('hidden')) {
+    readmeModal.classList.add('hidden');
+  } else if (!eatmeModal.classList.contains('hidden')) {
+    eatmeModal.classList.add('hidden');
+  } else if (!outModal.classList.contains('hidden')) {
+    outModal.classList.add('hidden');
+  } else {
+    console.log("Hérror, there isn't an open modal.");
+  }
+  bg.classList.add('hidden');
+  shadow.classList.add('hidden');
+  console.log('Close Modal');
+};
+////////////////////////////////
 ///////// EVENT LISTENER //////
+// CLOSE
+bg.addEventListener('click', closeModal);
+// OPEN
+pawn.addEventListener('click', openPawn);
+readme.addEventListener('click', openRead);
+eatme.addEventListener('click', openEat);
