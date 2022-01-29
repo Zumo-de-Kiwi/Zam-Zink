f549062e3620af3e59a7fc7d75d61d3821f0eab1
stonkol <hector96fi@yahoo.es>
2022-01-29 16:48:28 +0800

    ver. 0.690 bento home arduino
    1. now Hi changes his lang
    2. improve font-family var

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-// Close btn
+/////////////////////////////////
+///////// preloader /////////
+/////////////////////////////////
+window.addEventListener('load', () => {
+  const preloader = document.querySelector('.preloader');
+  preloader.classList.add('preload-finish');
+});
+
+/////////////////////////////////
+///////// Close btn /////////
+/////////////////////////////////
 const closeBtn = document.querySelector('.closeBtn');
 const footer = document.querySelector('.footer');
 
@@ -6,3 +16,21 @@ const closeFooter = function () {
   footer.classList.add('hidden');
 };
 closeBtn.addEventListener('click', closeFooter);
+
+/////////////////////////////////
+////////// HEY CHANGES ///////////
+/////////////////////////////////
+// VERSION D with fade
+$(function () {
+  count = 0;
+  // wordsArray = ['Hej', 'Hey', '哈囉', 'Hola', '哩賀'];
+  wordsArray = ['¡Hola!', '哈囉 !', 'Hej!', '哩賀 !', 'Hey!'];
+  setInterval(function () {
+    count++;
+    $('.change-hey').fadeOut(500, function () {
+      $(this)
+        .text(wordsArray[count % wordsArray.length])
+        .fadeIn(500);
+    });
+  }, 2300);
+});
