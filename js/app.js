843a64079e5deaa36ed305d0c9e94d05f3b6624a
stonkol <hector96fi@yahoo.es>
2025-04-09 15:35:21 +0800

    v0.97.3 Make home page bannermore concise. Compile app.js with ts.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
 //Function to get rid the preloader when the pages loads completely
-window.addEventListener('load', () => {
-  const preloader = document.querySelector('.preloader');
-  preloader.classList.add('preload-finish');
+window.addEventListener('load', function () {
+    var preloader = document.querySelector('.preloader');
+    preloader.classList.add('preload-finish');
 });
-
 // Smoth scrolling to a section
-const btnScrollTo = document.querySelector('.toTopBtn');
-const leftArrow = document.querySelector('.left-arrow');
+var btnScrollTo = document.querySelector('.toTopBtn');
+var leftArrow = document.querySelector('.left-arrow');
 btnScrollTo.addEventListener('click', function (e) {
-  leftArrow.scrollIntoView({ behavior: 'smooth' });
+    leftArrow.scrollIntoView({ behavior: 'smooth' });
 });
 // console.log(btnScrollTo, leftArrow);
-
 // Close btn
 // const closeBtn = document.querySelector('.closeBtn');
 // const footer = document.querySelector('.footer');
-
 // const closeFooter = function () {
 //   footer.classList.add('hidden');
 // };
