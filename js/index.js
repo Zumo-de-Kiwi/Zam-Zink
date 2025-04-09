4b9e5fdb93e6f3f19573d91adc5ac67879dcdeb2
stonkol <hector96fi@yahoo.es>
2025-04-09 14:47:17 +0800

    v0.97.1 rmv some old versions, try out the ts version

--- js/index.js
+++ js/index.js
@@ -1,141 +1,141 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jquery_1 = require("jquery");
 /////////////////////////////////
 /////// FOR HOME PAGE ///////////
-
 /////////////////////////////////
 /////////////////////////////////
 ///////// PRELOADER /////////////
 /////////////////////////////////
 /////////////////////////////////
-window.addEventListener('load', () => {
-  const preloader = document.querySelector('.preloader');
-  preloader.classList.add('preload-finish');
+window.addEventListener('load', function () {
+    var preloader = document.querySelector('.preloader');
+    if (preloader) {
+        preloader.classList.add('preload-finish');
+    }
 });
-
 /////////////////////////////////
 /////////////////////////////////
 ////////// DARK MODE  ///////////
 /////////////////////////////////
 /////////////////////////////////
 // https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]',
-);
-const currentTheme = localStorage.getItem('theme')
-  ? localStorage.getItem('theme')
-  : null;
+var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+var currentTheme = localStorage.getItem('theme')
+    ? localStorage.getItem('theme')
+    : null;
 /////////////////////////////////
 // 1. Check for saved user preference, if any, on load of the website
 if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-  }
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
 /////////////////////////////////
 // 2. Store the user preference for future visits
 // 3. Adding the event handlers (using the toogle)
 function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark'); //add this
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light'); //add this
-  }
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
 }
 toggleSwitch.addEventListener('change', switchTheme, false);
-
 /////////////////////////////////
 /////////////////////////////////
 ////////// HEY CHANGES //////////
 /////////////////////////////////
 /////////////////////////////////
 // VERSION D with fade
-$(function () {
-  count = 0;
-  // wordsArray = ['Hej', 'Hey', '哈囉', 'Hola', '哩賀'];
-  wordsArray = ['¡Hola!', '嗨~!', '👋', 'Hej!', '哩賀!', '🖖', 'Здарова!']; //'Hey' is already included //Other ru: Д'арова!
-  setInterval(function () {
-    count++;
-    $('.change-hey').fadeOut(404, function () {
-      $(this)
-        .text(wordsArray[count % wordsArray.length])
-        .fadeIn(404);
-    });
-  }, 3141);
+(0, jquery_1.default)(function () {
+    var count = 0;
+    var wordsArray = [];
+    wordsArray = ['¡Hola!', '嗨~!', '👋', 'Hej!', '哩賀!', '🖖', 'Здарова!']; //'Hey' is already included
+    setInterval(function () {
+        count++;
+        (0, jquery_1.default)('.change-hey').fadeOut(404, function () {
+            (0, jquery_1.default)(this)
+                .text(wordsArray[count % wordsArray.length])
+                .fadeIn(404);
+        });
+    }, 3141);
 });
-
 /////////////////////////////////
 /////////////////////////////////
 /////// POP UP STICKERS /////////
 /////////////////////////////////
 /////////////////////////////////
-const stickersLink = document.querySelector('#stickers-link');
-const stickersAll = document.querySelector('.stickers'); //all stickers pop up
-const bgClose = document.querySelector('.bg-close'); //bg-close
-const stickersX = document.querySelector('.stickers-x-btn');
-
+var stickersLink = document.querySelector('#stickers-link');
+var stickersAll = document.querySelector('.stickers'); //all stickers pop up
+var bgClose = document.querySelector('.bg-close'); //bg-close
+var stickersX = document.querySelector('.stickers-x-btn');
 //////////////// FUNC //////////////////
-const openStickers = function () {
-  stickersAll.classList.add('open');
-  bgClose.classList.remove('hidden');
+var openStickers = function () {
+    stickersAll === null || stickersAll === void 0 ? void 0 : stickersAll.classList.add('open');
+    bgClose === null || bgClose === void 0 ? void 0 : bgClose.classList.remove('hidden');
 };
-const closeStickers = function () {
-  stickersAll.classList.remove('open');
-  bgClose.classList.add('hidden');
+var closeStickers = function () {
+    stickersAll === null || stickersAll === void 0 ? void 0 : stickersAll.classList.remove('open');
+    bgClose === null || bgClose === void 0 ? void 0 : bgClose.classList.add('hidden');
 };
-
-let stickersOpen = 0;
-const openOrClose = function (whoClicked) {
-  console.log(whoClicked);
-  // whoclicked == 1 it need to be open
-  if (whoClicked == 1) {
-    stickersOpen = 1;
-    openStickers();
-  }
-  // whoclicked == 0 it need to be closed
-  else if (whoClicked == 0) {
-    stickersOpen = 0;
-  } else {
-    console.log('hérror on who clicked.');
-  }
+var stickersOpen = 0;
+var openOrClose = function (whoClicked) {
+    console.log(whoClicked);
+    // whoclicked == 1 it need to be open
+    if (whoClicked == 1) {
+        stickersOpen = 1;
+        openStickers();
+    }
+    // whoclicked == 0 it need to be closed
+    else if (whoClicked == 0) {
+        stickersOpen = 0;
+        // closeStickers();
+    }
+    else {
+        console.log('hérror on who clicked.');
+    }
 };
-
-const linkPressed = function () {
-  openOrClose(1);
+var linkPressed = function () {
+    openOrClose(1);
 };
-const bgPressed = function () {
-  openOrClose(0);
+var bgPressed = function () {
+    openOrClose(0);
 };
-const stickersXPressed = function () {
-  openOrClose(0);
+var stickersXPressed = function () {
+    openOrClose(0);
 };
-
-stickersLink.addEventListener('click', linkPressed);
-bgClose.addEventListener('click', closeStickers);
-stickersX.addEventListener('click', closeStickers);
+if (stickersLink) {
+    stickersLink.addEventListener('click', linkPressed);
+}
+if (bgClose) {
+    bgClose.addEventListener('click', closeStickers);
+}
+if (stickersX) {
+    stickersX.addEventListener('click', closeStickers);
+}
 // stickersX.addEventListener('click', stickersXPressed);
-
 /////////////////////////////////
 /////////////////////////////////
 ///////// Close btn /////////////
 /////////////////////////////////
 /////////////////////////////////
-const closeBtn = document.querySelector('.closeBtn');
-const footer = document.querySelector('.footer');
-
-const closeFooter = function () {
-  footer.classList.add('hidden');
+var closeBtn = document.querySelector('.closeBtn');
+var footer = document.querySelector('.footer');
+var closeFooter = function () {
+    footer === null || footer === void 0 ? void 0 : footer.classList.add('hidden');
 };
-closeBtn.addEventListener('click', closeFooter);
-//////////////////////////////////
-///// place left for MAC /////////
-if (navigator.userAgent.includes('Mac OS X')) {
-  closeBtn.classList.add('onLeft');
-}
-
+closeBtn === null || closeBtn === void 0 ? void 0 : closeBtn.addEventListener('click', closeFooter);
+// close button will be on the left when the user is using macOS
+// const isMac = navigator.userAgent.includes('Mac OS X');
+// if (isMac) {
+//   closeBtn?.classList.add('onLeft');
+// }
 // For other user system properties: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_nav_all
-console.log(`OS, Engine, Browser: ${navigator.userAgent}`);
-console.log(`Browser Language: ${navigator.language}`);
-console.log(`Cookies Enabled: ${navigator.cookieEnabled}`);
+console.log("OS, Engine, Browser: ".concat(navigator.userAgent));
+console.log("Browser Language: ".concat(navigator.language));
+console.log("Cookies Enabled: ".concat(navigator.cookieEnabled));
